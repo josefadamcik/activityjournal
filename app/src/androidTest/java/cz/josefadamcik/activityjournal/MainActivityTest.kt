fc4caa175774359877da0cda55449c407fa1a71a
Josef Adamcik <josef.adamcik@gmail.com>
2018-09-20 18:38:17 +0200

    First test working and the first activity created

---
+++ app/src/androidTest/java/cz/josefadamcik/activityjournal/MainActivityTest.kt
@@ -0,0 +1,30 @@
+package cz.josefadamcik.activityjournal
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainActivityTest {
+    @Rule
+    @JvmField
+    var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun applicationStart_activityDisplays() {
+        onView(withId(R.id.fab))
+                .check(matches(isDisplayed()))
+        onView(withId(R.id.toolbar))
+                .check(matches(isDisplayed()))
+    }
+
+}
