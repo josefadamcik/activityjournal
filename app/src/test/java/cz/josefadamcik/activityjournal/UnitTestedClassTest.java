0a39b348af239ac1852e9e9e3d0a921d6c9b574a
Josef Adamcik <josef.adamcik@gmail.com>
2018-09-20 21:27:56 +0200

    unit test for test coverage testing

--- app/src/test/java/cz/josefadamcik/activityjournal/UnitTestedClassTest.java
+++ app/src/test/java/cz/josefadamcik/activityjournal/UnitTestedClassTest.java
@@ -1,5 +1,15 @@
+package cz.josefadamcik.activityjournal;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class UnitTestedClassTest {
+
+	@Test
+	public void testcoveragetest() {
+		UnitTestedClass sut = new UnitTestedClass();
+		assertNotNull(sut.testcoveragetest());
+	}
 
 }
