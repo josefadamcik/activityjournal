cbe834d79abd98a51afca37d93f1781606b377ee
Josef Adamcik <josef.adamcik@gmail.com>
2018-09-24 15:58:17 +0200

    Converted to androdix

--- app/src/main/java/cz/josefadamcik/activityjournal/MainActivity.kt
+++ app/src/main/java/cz/josefadamcik/activityjournal/MainActivity.kt
@@ -1,9 +1,8 @@
 package cz.josefadamcik.activityjournal
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
-
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
